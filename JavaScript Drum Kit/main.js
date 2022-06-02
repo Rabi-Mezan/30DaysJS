0c7f3f62a53a5d2ab0c87ea6b42b7bbe87fcd7c7
Rabi-Mezan <ravimezan@gmail.com>
2022-06-02 21:39:48 +0600

    js drum kit done

--- JavaScript Drum Kit/main.js
+++ JavaScript Drum Kit/main.js
@@ -1,11 +1,30 @@
-window.addEventListener('keydown', function (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
 
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    console.log(key);
-    key.classList.add('playing')
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     if (!audio)
-        return
+        return;
     audio.currentTime = 0
+
+    //adding playing class to the pressed key for transition
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    key.classList.add('playing')
+
+
     audio.play()
-})
+
+}
+
+
+
+//remove the playing class from the pressed key after completion of the transition event
+function removeClass(e) {
+    if (e.propertyName !== 'transform')
+        return;
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeClass));
+
+
+window.addEventListener('keydown', playSound)
